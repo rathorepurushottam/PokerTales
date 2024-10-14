042313f24a73d172853c95b53fb99c281f20c820
Purushottam Rathore <purushottamrathore40@gmail.com>
2024-10-14 17:04:56 +0530

    minor changes

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  ImageBackground,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar
+        barStyle={'dark-content'}
+      />
+      <ImageBackground>
+
+      </ImageBackground>
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
