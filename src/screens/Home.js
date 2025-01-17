0f6a38da014158d81534d121d7db37d9c201fbaa
rathorepurushottam <purshottam@appinop.com>
2025-01-17 19:23:13 +0530

    home screen changes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -43,7 +43,11 @@ import {
   getUserWallet,
   upateTrackUser,
 } from "../actions/profileAction";
-import { ADD_CASH_SCREEN, LEADERBOARD_SCREEN, WEB_URL_SCREEN } from "../navigation/routes";
+import {
+  ADD_CASH_SCREEN,
+  LEADERBOARD_SCREEN,
+  WEB_URL_SCREEN,
+} from "../navigation/routes";
 import { getBrand } from "react-native-device-info";
 import RBSheet from "react-native-raw-bottom-sheet";
 import InviteAndEarn from "../common/InviteAndEarn";
@@ -57,12 +61,6 @@ const Home = () => {
   const userData = useSelector((state) => {
     return state.profile.userData;
   });
-
-  const initGame = useSelector((state) => {
-    return state.profile.initGame;
-  });
-
-  console.log(initGame, "initGame");
 
   // console.log(userData, "userData");
 
@@ -83,7 +81,7 @@ const Home = () => {
 
   const handleOpenEventBet = (type) => {
     dispatch(getInitGame(type));
-  }
+  };
 
   useEffect(() => {
     const windowWidth = Dimensions.get("window").width?.toFixed(2);
@@ -109,7 +107,7 @@ const Home = () => {
         handleAddCash={() => NavigationService.navigate(ADD_CASH_SCREEN)}
       />
       <ScrollView
-        contentContainerStyle={{flexGrow: 1}}
+        contentContainerStyle={{ flexGrow: 1 }} // Ensures the content can scroll if it exceeds the viewport
         refreshControl={
           <RefreshControl
             refreshing={refreshing}
@@ -118,103 +116,123 @@ const Home = () => {
           />
         }
       >
-        <View
-          style={{
-            backgroundColor: colors.bannerBackColor,
-            height: "30%",
-            justifyContent: "space-between",
-          }}
-        >
-          <FastImage
-            source={pokerTextIcon}
-            style={{ height: "50%", width: "100%" }}
-            resizeMode="center"
-          />
-          <FastImage
-            source={talesTextIcon}
-            style={{ height: "40%", width: "90%" }}
-            resizeMode="center"
-          />
-        </View>
-        <HomeSlider />
-        <View style={{flex: 1}}>
+        <View style={{ flex: 1 }}>
+          {/* Banner Section */}
           <View
             style={{
-              flexDirection: "row",
-              // justifyContent: "space-between",
-              paddingVertical: 15,
-              paddingHorizontal: 10,
-              width: "100%",
-              height: "50%",
-              // gap: 8
-            }}
-          >
-            <TouchableOpacity
-              onPress={() => handleOpenEventBet('cashGame')}
-              style={{ height: "100%", width: "50%", borderRadius: 10, overflow: "hidden" }}
-            >
-              <FastImage
-                source={cashGameIcon}
-                style={{ height: "100%", width: "100%" }}
-                resizeMode="contain"
-              />
-            </TouchableOpacity>
-            <TouchableOpacity style={{ height: "100%", width: "50%", borderRadius: 10, overflow: "hidden"  }} onPress={() => handleOpenEventBet('tournaments')}>
-              <FastImage
-                source={tournamentIcon}
-                style={{ height: "100%",width: "100%" }}
-              resizeMode="contain"
-              />
-            </TouchableOpacity>
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
+              backgroundColor: colors.bannerBackColor,
               justifyContent: "space-between",
-              paddingHorizontal: 10,
-              width: "100%",
-              height: "50%",
-              gap: 10
+              paddingVertical: 20,
             }}
           >
-              <TouchableOpacity
-              onPress={() => refRBSheetInvite.current.open()}
-              style={{ height: "60%", width: "50%", borderRadius: 10, overflow: "hidden" }}
+            <FastImage
+              source={pokerTextIcon}
+              style={{ height: 80, width: "100%" }}
+              resizeMode="center"
+            />
+            <FastImage
+              source={talesTextIcon}
+              style={{ height: 80, width: "90%", alignSelf: "center" }}
+              resizeMode="center"
+            />
+          </View>
+
+          {/* Slider Section */}
+          <HomeSlider />
+
+          {/* Feature Buttons Section */}
+          <View style={{ padding: 10 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginBottom: 15,
+              }}
             >
-              <FastImage
-                source={referHomeIcon}
-                style={{ height: "100%", width: "100%" }}
-                resizeMode="contain"
-              />
-            </TouchableOpacity>
-            <View style={{ height: "60%", width: "50%", gap: 5}}>
-              <TouchableOpacity style={{ height: "50%", width: "100%" }} onPress={() => NavigationService.navigate(LEADERBOARD_SCREEN)}>
-              <FastImage
-                source={leaderBoardIcon}
-                style={{ height: "100%", width: "90%" }}
-               resizeMode="contain"
-              />
-              </TouchableOpacity>
-              <TouchableOpacity style={{ height: "50%", width: "90%" }}>
+              <TouchableOpacity
+                onPress={() => handleOpenEventBet("cashGame")}
+                style={{
+                  height: 220,
+                  width: "48%",
+                  borderRadius: 10,
+                  overflow: "hidden",
+                }}
+              >
                 <FastImage
-                  source={tournamentLeaderIcon}
+                  source={cashGameIcon}
                   style={{ height: "100%", width: "100%" }}
-                 resizeMode="contain"
+                  resizeMode="contain"
                 />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => handleOpenEventBet("tournaments")}
+                style={{
+                  height: 220,
+                  width: "48%",
+                  borderRadius: 10,
+                  overflow: "hidden",
+                }}
+              >
+                <FastImage
+                  source={tournamentIcon}
+                  style={{ height: "100%", width: "100%" }}
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
+              <TouchableOpacity
+                onPress={() => refRBSheetInvite.current.open()}
+                style={{
+                  height: 150,
+                  width: "48%",
+                  borderRadius: 10,
+                  overflow: "hidden",
+                }}
+              >
+                <FastImage
+                  source={referHomeIcon}
+                  style={{ height: "100%", width: "100%" }}
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+              <View style={{ width: "48%" }}>
+                <TouchableOpacity
+                  style={{ height: 70, marginBottom: 10 }}
+                  onPress={() => NavigationService.navigate(LEADERBOARD_SCREEN)}
+                >
+                  <FastImage
+                    source={leaderBoardIcon}
+                    style={{ height: "100%", width: "100%" }}
+                    resizeMode="contain"
+                  />
                 </TouchableOpacity>
+                <TouchableOpacity style={{ height: 70 }}>
+                  <FastImage
+                    source={tournamentLeaderIcon}
+                    style={{ height: "100%", width: "100%" }}
+                    resizeMode="contain"
+                  />
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-        
+
+          {/* Banner Bottom Section */}
           <View style={styles.bannerBottomView}>
             <AppText style={{ color: "#FFFFFF1A" }}>BANNER HERE</AppText>
           </View>
           <FastImage
             source={homeFooterIcon}
-            resizeMode="center"
-            style={{ width: 400, height: 330 }}
+            resizeMode="contain"
+            style={{ width: "100%", height: 250, marginTop: 20 }}
           />
         </View>
       </ScrollView>
+
       <RBSheet
         ref={refRBSheetSignupBonus}
         closeOnDragDown={true}
@@ -283,7 +301,10 @@ const Home = () => {
           },
         }}
       >
-        <InviteAndEarn code={userData?.referralCode} onCloseInvite={handleCloseInvite}/>
+        <InviteAndEarn
+          code={userData?.referralCode}
+          onCloseInvite={handleCloseInvite}
+        />
       </RBSheet>
     </AppSafeAreaView>
   );
@@ -307,7 +328,7 @@ const styles = StyleSheet.create({
     // height: 66,
     // width: 390,
     width: "95%",
-    height: "10%",
+    height: 50,
     justifyContent: "center",
     alignItems: "center",
     borderWidth: 1,
